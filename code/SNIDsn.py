7ee6108f4a5f3a906d00742e56a715c8b1817bad
Marc Williamson <marcwilliamson@10-17-78-123.dynapool.wireless.nyu.edu>
2022-05-03 11:17:18 -0400

    added write_lnw function to save SNIDsn objects as SNID template files.

--- code/SNIDsn.py
+++ code/SNIDsn.py
@@ -693,6 +693,103 @@ class SNIDsn:
         self.continuum = continuum
         return
 
+    def write_lnw(self, filename):
+        """
+        Writes the data of the SNIDsn object to an output file using the
+        SNID .lnw format. Note that the continuum information is copied
+        from the SNIDsn header, which is not updated if the user makes
+        changes to the spectra. Modified spectra may have inconsistent
+        continuum information. Users should exercise extreme care to modify
+        all relevant SNIDsn header properties before calling this function
+        in order to write consistent .lnw files.
+
+        Parameters
+        ----------
+        filename : string
+            Path to write output file to.
+
+        Returns
+        -------
+
+        """
+        file_lines = []
+        header_items = []
+        header_items.append('   ' + str(self.header['Nspec']))
+        header_items.append(' ' + str(self.header['Nbins']))
+        header_items.append('   ' + str('{:.2f}'.format(self.header['WvlStart'])))
+        header_items.append('  ' + str('{:.2f}'.format(self.header['WvlEnd'])))
+        header_items.append('     ' + str(self.header['SplineKnots']))
+        header_items.append('     ' + str(self.header['SN']))
+        header_items.append('      ' + str(self.header['dm15']))
+        header_items.append('  ' + str(self.header['TypeStr']))
+        header_items.append('     ' + str(self.header['TypeInt']))
+        header_items.append('  ' + str(self.header['SubTypeInt']))
+        header_line = ''
+        for item in header_items:
+            header_line += item
+        file_lines.append(header_line)
+        continuum = self.continuum.tolist()
+        continuum_header = continuum[0]
+        continuum_line = ''
+        for i in range(len(continuum_header)):
+            if float(continuum_header[i]) == int(continuum_header[i]):
+                item = int(continuum_header[i])
+            else:
+                item = continuum_header[i]
+            if i == 0:
+                continuum_line += '     ' + str(item)
+            elif i % 2 == 0:
+                continuum_line += '       ' + str('{:.5f}'.format(item))
+            else:
+                if item >= 10:
+                    continuum_line += ' ' + str(item)
+                else:
+                    continuum_line += '  ' + str(item)
+        file_lines.append(continuum_line)
+        continuum_all = ''
+        for i in range(1, len(continuum)):
+            for j in range(len(continuum[i])):
+                item = str('{:.4f}'.format(continuum[i][j]))
+                if j == 0:
+                    continuum_all += '      ' + str(i)
+                else:
+                    if j % 2 == 0 and float(item) > 0:
+                        continuum_all += '   ' + item
+                    else:
+                        continuum_all += '  ' + item
+            file_lines.append(continuum_all)
+            continuum_all = ''
+        phases = ['       0']
+        str_phase = self.data.dtype.names
+        for phase in str_phase:
+            if float(phase[2:]) < 100:
+                phases.append('   ' + str('{:.3f}'.format(float(phase[2:]))))
+            else:
+                phases.append('  ' + str('{:.3f}'.format(float(phase[2:]))))
+        file_lines.append(phases)
+        data = self.data.tolist()
+        wvl = self.wavelengths
+        count = 0
+        for line in data:
+            fluxes = []
+            fluxes.append(' ' + str('{:.2f}'.format(wvl[count], 2)))
+            for i in range(len(line)):
+                if line[i] >= 0:
+                    fluxes.append('    ' + str('{:.3f}'.format(line[i], 3)))
+                else:
+                    fluxes.append('   ' + str('{:.3f}'.format(line[i], 3)))
+            count += 1
+            file_lines.append(fluxes)
+        with open(filename, 'x') as lnw:
+            for line in file_lines:
+                for i in range(len(line)):
+                    lnw.write(line[i])
+                lnw.write('\n')
+            lnw.close()
+
+        return
+
+
     def preprocess(self, phasekey):
         """
         Zeros the mean and scales std to 1 for the spectrum indicated.
